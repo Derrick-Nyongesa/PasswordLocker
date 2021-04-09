0d2b05c65a0b6f221f44affd008d6b7ecf3dcaf0
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-09 07:09:20 +0300

    subclass TestUser created

--- password_test.py
+++ password_test.py
@@ -1,2 +1,12 @@
 import unittest
 from password import User
+
+class TestUser(unittest.TestCase):
+    """
+    Test class that defines the test cases for the user class behaviours.
+
+    Args:
+        unittest: TestCase class that helps in creating test cases.
+    """
+
+    

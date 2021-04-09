0edbab6ee58fe213336c64291038d20792366ac8
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-09 07:27:09 +0300

    tested whether the objects are being instantiated correctly

--- password_test.py
+++ password_test.py
@@ -9,4 +9,18 @@ class TestUser(unittest.TestCase):
         unittest: TestCase class that helps in creating test cases.
     """
 
-    
+    def setUp(self):
+        """
+        Set up method before each test cases.
+        """
+        self.new_user = User("Derrick-Nyongesa", "DN17w9S")
+
+    def test_init(self):
+        """
+        This function tests if the object is initialized properly
+        """
+        self.assertEqual(self.new_user.username, "Derrick-Nyongesa")
+        self.assertEqual(self.new_user.password, "DN17w9S")
+
+if __name__ == '__main__':
+    unittest.main()

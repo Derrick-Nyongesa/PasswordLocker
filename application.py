df58ac4e665afa71501dc64a313bb582f562614f
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-09 11:42:27 +0300

    created a function to create a new user account

---
+++ application.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.9
+
+from password import User
+from password import Credential
+
+def create_user(username, password):
+    """
+    Function to create a new user account
+
+    Args:
+        username: New username
+        password : New password
+    """
+    new_user = User(username, password)
+    return new_user

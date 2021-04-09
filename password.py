6216f354b00f12fa1f7f9e0e7a0fefdb2da48174
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-09 07:12:06 +0300

    user list to store user objects

--- password.py
+++ password.py
@@ -2,6 +2,9 @@ class User:
     """
     Class that generates new instances of the user
     """
+
+    user_list = []
+    
     def __init__(self, username, password):
         """
         Method to define the properties for each user object will hold.
